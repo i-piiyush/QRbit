05b99cbdf556243074f0ef02bfc23c3804170ea0
i-piiyush <piyushchhabra3578@gmail.com>
2025-03-14 20:27:07 +0530

    commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -19,14 +19,14 @@ const Navbar = () => {
     };
 
     window.addEventListener("scroll", handleScroll);
-    
+
     return () => {
       window.removeEventListener("scroll", handleScroll);
       clearTimeout(scrollTimeout);
     };
   }, []);
 
-return (
+  return (
     <motion.div
       className=" fixed bottom-[5%] left-0 w-full flex justify-center items-center  z-50 text-white text-center"
       initial={{ opacity: 0 }}
@@ -34,20 +34,20 @@ return (
       transition={{ duration: 0.5 }}
     >
       <nav className="flex gap-1 bg-green-950/85 rounded-md w-80 h-20 backdrop-blur-md shadow-[7px_7px_59px_11px_rgba(0,0,0,0.5)] p-[5px]">
-      <div className="bg-white h-full w-[25%] rounded-lg flex justify-center items-center text-3xl text-black  font-semibold">
-        <h1 className="text-green-400">Qb</h1>.
-      </div>
-      <div className="border border-green-400/15 hover:border-green-400 transition-all duration-300 h-full w-[25%] rounded-lg flex justify-center items-center text-md text-white font-medium cursor-pointer">
-  <div>Home</div>
-</div>
-<div className="border border-green-400/15 hover:border-green-400 transition-all duration-300 h-full w-[25%] rounded-lg flex justify-center items-center text-md text-white font-medium cursor-pointer">
-  <div>Features</div>
-</div>
+        <div className="bg-white h-full w-[25%] rounded-lg flex justify-center items-center text-3xl text-black  font-semibold">
+          <h1 className="text-green-400">Qb</h1>.
+        </div>
+        <div className="border border-green-400/15 hover:border-green-400 transition-all duration-300 h-full w-[25%] rounded-lg flex justify-center items-center text-md text-white font-medium cursor-pointer">
+          <a href="#home">Home</a>
+        </div>
+        <div className="border border-green-400/15 hover:border-green-400 transition-all duration-300 h-full w-[25%] rounded-lg flex justify-center items-center text-md text-white font-medium cursor-pointer">
+          <a  href="#features">Features</a>
+        </div>
 
-      <div className="bg-green-400 h-full w-[25%] rounded-lg flex justify-center items-center text-md text-white  font-medium"> <a href="#">Sign-up</a> </div>
-
-      
-
+        <div className="bg-green-400 h-full w-[25%] rounded-lg flex justify-center items-center text-md text-white  font-medium">
+          {" "}
+          <a href="#">Sign-up</a>{" "}
+        </div>
       </nav>
     </motion.div>
   );

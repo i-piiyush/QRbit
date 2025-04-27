43706fcf9e97840c31ff0de6f5fe17eca0986c2c
Piyush <[email protected]>
2025-04-28 00:12:10 +0530

    wokring on userCards

--- src/Components/UserCards.jsx
+++ src/Components/UserCards.jsx
@@ -1,35 +1,65 @@
-import React, { useContext } from "react";
-import { AppContext } from "../context/AppProvider";
-import Card1 from "./Card1";
-import Card2 from "./Card2";
-import Card3 from "./Card3";
+import React, { useEffect, useState } from "react";
+import { ArrowUpRight, LoaderIcon } from "lucide-react";
+import { collection, doc, getDoc } from "firebase/firestore";
+import { getAuth } from "firebase/auth"; // 👈 import auth
+import { db } from "../firebaseConfig";
+
 
 const UserCards = () => {
-  const {selectedCardIndex} = useContext(AppContext)
-  console.log(selectedCardIndex);
-  
-  return (
-    
-    <>
-      <div className="bg-red-400 w-full h-[20vh] flex justify-center items-center">
-        <h1 className="text-5xl font-bold ">Your Card</h1>
-      </div>
-      <div className="bg-blue-300 w-full px-5 py-3">
-        <div className="w-[350px] ">
+  const [user, setUser] = useState(null);
 
-        {
-    selectedCardIndex === 1 ? (
-      <Card1 />
-    ) : selectedCardIndex === 2 ? (
-      <Card2 />
-    ) : (
-      <Card3 />
-    )
-  }
-        </div>
-    
+  useEffect(() => {
+    const fetchData = async () => {
+      const auth = getAuth();
+      const currentUser = auth.currentUser;
+
+      if (!currentUser) {
+        console.log("No user logged in");
+        return;
+      }
+
+      const userDocRef = doc(db, "users", currentUser.uid);
+      const userDocSnap = await getDoc(userDocRef);
+
+      if (userDocSnap.exists()) {
+        setUser({ id: userDocSnap.id, ...userDocSnap.data() });
+        console.log({ id: userDocSnap.id, ...userDocSnap.data() });
+      } else {
+        console.log("No such user document!");
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className=" w-full h-screen px-8 py-6  relative ">
+      <div className="blob top-[12rem] h-[300px] w-[300px] -left-[5rem]"></div>
+      <div className=" flex flex-col justify-between h-[20vh] relative z-10">
+        <h1 className="text-white text-4xl font-bold">{`Hey ${user.name},`}</h1>
+
+
+        <p className="font-semibold text-center text-white text-lg opacity-75">Take your next steps:</p>
+
+
       </div>
-    </>
+      <div className="mt-10 flex flex-col gap-3 relative z-10">
+        <div className="h-44 flex gap-3 px-2">
+          <div className="bg-black h-full w-1/2 rounded"></div>
+          <div className="bg-black h-full w-1/2 rounded"></div>
+        </div>
+        <div className="h-28 px-2">
+
+          <div className="bg-black h-full rounded"></div>
+          
+        </div>
+        <div className="h-32 px-2">
+
+          <div className="bg-black h-full rounded"></div>
+          
+        </div>
+      </div>
+    </div>
   );
 };
 
